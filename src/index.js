4ffe0ac419010d964c2543d55ef407780deb699e
sizeight <hpretorius@gmail.com>
2018-12-10 16:09:38 +0200

    Bring in code

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import * as actions from './actions';
+import * as reducer from './reducer';
+import * as selectors from './selectors';
+import * as utils from './utils';
+
+export {
+  actions, reducer, selectors, utils,
+};
---
+++ webpack.config.js
@@ -0,0 +1,105 @@
+const path = require('path');
+const webpack = require('webpack');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+const nodeEnv = process.env.NODE_ENV === 'production' ? 'production' : 'development';
+const isProduction = nodeEnv === 'production';
+
+const sourcePath = path.join(__dirname, './src/');
+const buildPath = path.join(__dirname, './lib/');
+
+// Common plugins
+const plugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: JSON.stringify(nodeEnv),
+      API_URL: isProduction
+        ? JSON.stringify('') : JSON.stringify('http://127.0.0.1:8000'),
+    },
+  }),
+];
+
+if (process.env.ANALYSE === 'true') {
+  plugins.push(new BundleAnalyzerPlugin());
+}
+
+module.exports = {
+  mode: nodeEnv,
+  devtool: isProduction ? false : 'source-map',
+  context: sourcePath,
+  entry: {
+    'redux-scaffolding': `${sourcePath}index.js`,
+  },
+  output: {
+    path: buildPath,
+    filename: isProduction ? '[name].min.js' : '[name].js',
+    libraryTarget: 'commonjs2',
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        use: [
+          'eslint-loader',
+        ],
+      },
+      {
+        test: /\.js$/,
+        use: [
+          'babel-loader',
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  plugins,
+  externals: [
+    {
+      'react-dom': {
+        root: 'ReactDOM',
+        commonjs2: 'react-dom',
+        commonjs: 'react-dom',
+        amd: 'react-dom',
+      },
+    },
+    {
+      react: {
+        root: 'React',
+        commonjs2: 'react',
+        commonjs: 'react',
+        amd: 'react',
+      },
+    },
+    {
+      'prop-types': {
+        root: 'prop-types',
+        commonjs2: 'prop-types',
+        commonjs: 'prop-types',
+        amd: 'prop-types',
+      },
+    },
+    {
+      reactstrap: {
+        root: 'reactstrap',
+        commonjs2: 'reactstrap',
+        commonjs: 'reactstrap',
+        amd: 'reactstrap',
+      },
+    },
+    {
+      'isomorphic-fetch': {
+        root: 'isomorphic-fetch',
+        commonjs2: 'isomorphic-fetch',
+        commonjs: 'isomorphic-fetch',
+        amd: 'isomorphic-fetch',
+      },
+    },
+  ],
+};
