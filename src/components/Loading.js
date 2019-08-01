c18af7abd2e7dc196ff8636035b0533a595a1f4d
sizeight <hpretorius@gmail.com>
2019-08-01 10:05:41 +0200

    Issue #29

---
+++ src/components/Loading.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './Loading.css';
+
+
+const propTypes = {
+  animate: PropTypes.bool,
+  size: PropTypes.oneOf(['1x', '2x', '3x']),
+  center: PropTypes.bool,
+};
+
+const defaultProps = {
+  animate: false,
+  size: '1x',
+  center: false,
+};
+
+const Loading = (props) => {
+  const { animate, size, center } = props;
+
+  const wrapperClass = `redux-scaffolding-loader-wrapper${center ? ' redux-scaffolding-loader-center' : ''}`;
+  const loaderClass = `redux-scaffolding-loader redux-scaffolding-loader-size-${size}`;
+
+  return (
+    <div className={wrapperClass}>
+      {animate
+        ? (
+          <div className={loaderClass} />
+        ) : (
+          <em>Loading&hellip;</em>
+        )}
+    </div>
+  );
+};
+
+Loading.propTypes = propTypes;
+Loading.defaultProps = defaultProps;
+
+export default Loading;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import * as actions from './actions';
 import * as reducer from './reducer';
 import * as selectors from './selectors';
 import * as utils from './utils';
+import Loading from './components/Loading';
 
 export {
-  actions, reducer, selectors, utils,
+  actions, reducer, selectors, utils, Loading,
 };
