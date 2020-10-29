379a4b32c44955c3c71d6bcecf3a4fe84335ede4
sizeight <hpretorius@gmail.com>
2020-10-29 10:11:10 +0200

    Issue #40

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import * as reducer from './reducer';
 import * as selectors from './selectors';
 import * as utils from './utils';
 import useFilter from './hooks/useFilter';
+import useSort from './hooks/useSort';
 import Loading from './components/Loading';
 
 export {
-  actions, reducer, selectors, utils, useFilter, Loading,
+  actions, reducer, selectors, utils, useFilter, useSort, Loading,
 };
